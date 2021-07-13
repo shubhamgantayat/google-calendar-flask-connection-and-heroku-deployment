e8f6f8c94c88cf1c65fcb5b04816cfa5ac854b6c
Shubham Gantayat <sgantayat9@gmail.com>
2021-07-13 19:11:05 +0530

    sixth commit

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import datetime
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 
 app = Flask(__name__)
 
@@ -31,37 +29,24 @@ def calendar():
         auth_code = flask.request.args.get('code')
         flow.fetch_token(code=auth_code)
         creds = flow.credentials
+        service = build('calendar', 'v3', credentials=creds)
 
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        events_result = service.events().list(calendarId='addressbook#contacts@group.v.calendar.google.com',
+                                              timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
 
-
-    # port = int(os.environ.get('PORT', 8080))
-    # creds = flow.run_local_server(host='0.0.0.0', port=port)
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='addressbook#contacts@group.v.calendar.google.com',
-                                          timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        string = 'No upcoming events found.'
-    result = []
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        e = dict(event)
-        if 'summary' in e.keys():
-            result.append([start, e['summary']])
-    string = result
-    # except ValueError:
-    #     string = 'Please enter an integer'
-    # except TypeError:
-    #     string = 'Please enter an integer greater than zero'
-    # finally:
-    return render_template('form.html',results = string)
+        result = []
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            e = dict(event)
+            if 'summary' in e.keys():
+                result.append([start, e['summary']])
+        string = '\n'.join(result) if len(result) != 0 else 'No birthdays found on your google calendar'
+        return render_template('form.html',results = string)
 
 
 if __name__ == '__main__':
